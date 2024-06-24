74fd25118b66627555390020a5d31f06357d5393
Halid Besic <halidstl@gmail.com>
2024-06-23 22:54:17 -0500

    2 completed

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -2,13 +2,20 @@ let food = "water bottles,meal packs,snacks,chocolate";
 let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
-
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
-
+cabinetOne = food.split(',').sort();
+cabinetTwo = equipment.split(',').sort();
+cabinetThree = pets.split(',').sort();
+cabinetFour = sleepAids.split(',').sort();
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
-
+let cargoHold = [cabinetOne, cabinetTwo, cabinetThree, cabinetFour];
+for (let i = 0; i < cargoHold.length; i++) {
+    console.log(cargoHold[i]);
+}
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+let userInput = prompt("Please select a cabinet (0 - 3): ");
+
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-
+//let cargoOutput  = ""
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -4,8 +4,29 @@ let str = "LaunchCode";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+
+//let newString = str.slice(3) + str.slice(0,3);
+
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+
+
+//console.log(` 'We change ${str} into ${newString} using string methods'`);
+
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+let numLetters = Number(input.question("How many letters will be location? "));
+
+let newString = str.slice(numLetters) + str.slice(0, numLetters);
+
+console.log(newString);
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (numLetters > str.length){
+    console.log("Input exceeded length of the word.");
+    let defaultString = str.slice(3) + str.slice(0,3);
+    console.log(` 'We change ${str} into ${defaultString} using string methods'`);
+} else {
+    console.log(` 'We change ${str} into ${newString} using string methods'`);
+}
